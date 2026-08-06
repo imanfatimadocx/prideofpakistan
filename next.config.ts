1fc58d894cc79e7c8c46b67aa286fae22c4884ea
imanfatimadocx <imanwardag@gmail.com>
2026-08-06 14:39:59 +0500

    Add Cloudinary and image domains to Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'prideofpakistan.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
